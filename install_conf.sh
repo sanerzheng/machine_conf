eddac611c2cbe749a8c9c546b4d4d7510ca070cc
saner zheng <sanerzheng@gmail.com>
2017-12-29 16:54:47 +0800

    fix

--- install_conf.sh
+++ install_conf.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)
 git clone https://github.com/seebi/dircolors-solarized.git ~/.dircolors
 mv ~/.bashrc ~/bashrc_bak
-cp bash_conf ~/.bashrc
-cp vimrc ~/.vim_runtime
+cp -r bash_conf ~/.bashrc
+cp -r vimrc ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 echo "Input 'source ~/.bashrc' to activate bash configuration."
